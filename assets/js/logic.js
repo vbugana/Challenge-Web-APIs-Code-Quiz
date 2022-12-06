34eef5de972bb506272365572462bdec9f164db2
vbugana <61995940+vbugana@users.noreply.github.com>
2022-12-06 01:29:27 +0000

    updated Javascript, try to clean the code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,23 +1,24 @@
 //select each card div by id and assign to variables
-var startCard = document.querySelector("#start-card");
-var questionCard = document.querySelector("#question-card");
-var scoreCard = document.querySelector("#score-card");
-var leaderboardCard = document.querySelector("#leaderboard-card");
+var startCard = document.querySelector('#start-card');
+var questionCard = document.querySelector('#question-card');
+var scoreCard = document.querySelector('#score-card');
+var leaderBoardCard = document.querySelector('#leaderboard-card');
+var questions;
 
 //hide all cards
 function hideCards() {
-  startCard.setAttribute("hidden", true);
-  questionCard.setAttribute("hidden", true);
-  scoreCard.setAttribute("hidden", true);
-  leaderboardCard.setAttribute("hidden", true);
+	startCard.setAttribute('hidden', true);
+	questionCard.setAttribute('hidden', true);
+	scoreCard.setAttribute('hidden', true);
+	leaderBoardCard.setAttribute('hidden', true);
 }
 
-var resultDiv = document.querySelector("#result-div");
-var resultText = document.querySelector("#result-text");
+var resultDiv = document.querySelector('#result-div');
+var resultText = document.querySelector('#result-text');
 
 //hide result div
 function hideResultText() {
-  resultDiv.style.display = "none";
+	resultDiv.style.display = 'none';
 }
 
 //these variables are required globally
@@ -25,220 +26,267 @@ var intervalID;
 var time;
 var currentQuestion;
 
-document.querySelector("#start-button").addEventListener("click", startQuiz);
+document.querySelector('#start-button').addEventListener('click', startQuiz);
 
 function startQuiz() {
-  //hide any visible cards, show the question card
-  hideCards();
-  questionCard.removeAttribute("hidden");
+	//hide any visible cards, show the question card
+	hideCards();
+	questionCard.removeAttribute('hidden');
 
-  //assign 0 to currentQuestion when start button is clicked, then display the current question on the page
-  currentQuestion = 0;
-  displayQuestion();
+	//assign 0 to currentQuestion when start button is clicked, then display the current question on the page
+	currentQuestion = 0;
+	displayQuestion();
 
-  //set total time depending on number of questions
-  time = questions.length * 10;
+	//set total time depending on number of questions
+	time = questions.length * 15;
 
-  //executes function "countdown" every 1000ms to update time and display on page
-  intervalID = setInterval(countdown, 1000);
+	//executes function 'countdown' every 1000ms to update time and display on page
+	intervalID = setInterval(countdown, 1000);
 
-  //invoke displayTime here to ensure time appears on the page as soon as the start button is clicked, not after 1 second
-  displayTime();
+	//invoke displayTime here to ensure time appears on the page as soon as the start button is clicked, not after 1 second
+	displayTime();
 }
 
 //reduce time by 10 and display new value, if time runs out then end quiz
 function countdown() {
-  time--;
-  displayTime();
-  if (time < 10) {
-    endQuiz();
-  }
+	time--;
+	displayTime();
+	if (time < 10) {
+		endQuiz();
+	}
 }
 
 //display time on page
-var  timeDisplay = document.querySelector("#time");
+var timeDisplay = document.querySelector('#time');
+
 function displayTime() {
-  timeDisplay.textContent = time;
+	timeDisplay.textContent = time;
 }
 
 //display the question and answer options for the current question
 function displayQuestion() {
-  var  question = questions[currentQuestion];
-  var  options = question.options;
+	var question = questions[currentQuestion];
+	var options = question.options;
 
-  var  h2QuestionElement = document.querySelector("#question-text");
-  h2QuestionElement.textContent = question.questionText;
+	var h2QuestionElement = document.querySelector('#question-text');
+	h2QuestionElement.textContent = question.questionText;
 
-  for (var  i = 0; i < options.length; i++) {
-    var  option = options[i];
-    var  optionButton = document.querySelector("#option" + i);
-    optionButton.textContent = option;
-  }
+	for (var i = 0; i < options.length; i++) {
+		var option = options[i];
+		var optionButton = document.querySelector('#option' + i);
+		optionButton.textContent = option;
+	}
+
 }
 
-//behaviour when an answer button is clicked: click event bubbles up to div with id "quiz-options"
-//eventObject.target identifies the specific button element that was clicked on
-document.querySelector("#quiz-options").addEventListener("click", checkAnswer);
+//behaviour when an answer button is clicked: click event bubbles up to div with id 'quiz-options'
+document.querySelector('#quiz-options').addEventListener('click', checkAnswer);
 
 //Compare the text content of the option button with the answer to the current question
 function optionIsCorrect(optionButton) {
-  return optionButton.textContent === questions[currentQuestion].answer;
+	return optionButton.textContent === questions[currentQuestion].answer;
 }
 
 //if answer is incorrect, penalise time
 function checkAnswer(eventObject) {
-  var  optionButton = eventObject.target;
-  resultDiv.style.display = "block";
-  if (optionIsCorrect(optionButton)) {
-    resultText.textContent = "Correct!";
-    setTimeout(hideResultText, 1000);
-  } else {
-    resultText.textContent = "Incorrect!";
-    setTimeout(hideResultText, 1000);
-    if (time >= 10) {
-      time = time - 10;
-      displayTime();
-    } else {
-      //if time is less than 10, display time as 0 and end quiz
-      //time is set to zero in this case to avoid displaying a negative number in cases where a wrong answer is submitted with < 10 seconds left on the timer
-      time = 0;
-      displayTime();
-      endQuiz();
-    }
-  }
+	var optionButton = eventObject.target;
+	var soundCorrect = new Audio('.assets/sfx/correct.wav');
+	var soundIncorrect = new Audio('.assets/sfx/incorrect.wav');
+	resultDiv.style.display = 'block';
+	if (optionIsCorrect(optionButton)) {
+		resultText.textContent = 'Correct!';
+		setTimeout(hideResultText, 1000);
+		soundCorrect.play();
+	} else {
+		resultText.textContent = 'Incorrect!';
+		soundIncorrect.play();
+		setTimeout(hideResultText, 1000);
+		if (time >= 10) {
+			time = time - 10;
+			displayTime();
+		} else {
+			//if time is less than 10, display time as 0 and end quiz
+			//time is set to zero in this case to avoid displaying a negative number in cases where a wrong answer is submitted with < 10 seconds left on the timer
+			time = 0;
+			displayTime();
+			endQuiz();
+		}
+	}
 
-  //increment current question by 1
-  currentQuestion++;
-  //if we have not run out of questions then display next question, else end quiz
-  if (currentQuestion < questions.length) {
-    displayQuestion();
-  } else {
-    endQuiz();
-  }
+	//increment current question by 1
+	currentQuestion++;
+	//if we have not run out of questions then display next question, else end quiz
+	if (currentQuestion < questions.length) {
+		displayQuestion();
+	} else {
+		endQuiz();
+	}
 }
-
-//display scorecard and hide other divs
-var  score = document.querySelector("#score");
-
-//at end of quiz, clear the timer, hide any visible cards and display the scorecard and display the score as the remaining time
-function endQuiz() {
-  clearInterval(intervalID);
-  hideCards();
-  scoreCard.removeAttribute("hidden");
-  score.textContent = time;
-}
-
-var  submitButton = document.querySelector("#submit-button");
-var  inputElement = document.querySelector("#initials");
-
-//store user initials and score when submit button is clicked
-submitButton.addEventListener("click", storeScore);
 
 function storeScore(event) {
-  //prevent default behaviour of form submission
-  event.preventDefault();
+	//prevent default behaviour of form submission
+	event.preventDefault();
 
-  //check for input
-  if (!inputElement.value) {
-    alert("Please enter your initials before pressing submit!");
-    return;
-  }
+	//check for input
+	if (!inputElement.value) {
+		alert('Please enter your initials before pressing submit!');
+		return;
+	}
+	//store score and initials in an object
+	var leaderBoardItem = {
+		initials: inputElement.value,
+		score: time,
+	};
 
-  //store score and initials in an object
-  var  leaderboardItem = {
-    initials: inputElement.value,
-    score: time,
-  };
+	updateStoredLeaderBoard(leaderBoardItem);
 
-  updateStoredLeaderboard(leaderboardItem);
+	//hide the question card, display the leader's board card
+	hideCards();
+	leaderBoardCard.removeAttribute('hidden');
 
-  //hide the question card, display the leaderboardcard
-  hideCards();
-  leaderboardCard.removeAttribute("hidden");
+	renderLeaderBoard();
 
-  renderLeaderboard();
+	//updates the leader's board stored in local storage
+	function updateStoredLeaderBoard(leaderBoardItem) {
+		var leaderBoardArray = getLeaderBoard();
+		//append new leader's board item to leader's board array
+		leaderBoardArray.push(leaderBoardItem);
+		localStorage.setItem('leaderBoardArray', JSON.stringify(leaderBoardArray));
+	}
+
+	//get 'leaderBoardArray' from local storage (if it exists) and parse it into a javascript object using JSON.parse
+	function getLeaderBoard() {
+		var storedLeaderBoard = localStorage.getItem('leaderBoardArray');
+		if (storedLeaderBoard !== null) {
+			var leaderBoardArray = JSON.parse(storedLeaderBoard);
+			return leaderBoardArray;
+		} else {
+			leaderBoardArray = [];
+		}
+		return leaderBoardArray;
+	}
 }
 
-//updates the leaderboard stored in local storage
-function updateStoredLeaderboard(leaderboardItem) {
-  var  leaderboardArray = getLeaderboard();
-  //append new leaderboard item to leaderboard array
-  leaderboardArray.push(leaderboardItem);
-  localStorage.setItem("leaderboardArray", JSON.stringify(leaderboardArray));
+//display leader's board on leader's board card
+function renderLeaderBoard() {
+	var sortedLeaderBoardArray = sortLeaderBoard();
+	var scoreList = document.querySelector('#highscore-list');
+	scoreList.innerHTML = '';
+	for (var i = 0; i < sortedLeaderBoardArray.length; i++) {
+		var leaderBoardEntry = sortedLeaderBoardArray[i];
+		var newListItem = document.createElement('li');
+		newListItem.textContent =
+			leaderBoardEntry.initials + ' - ' + leaderBoardEntry.score;
+		scoreList.append(newListItem);
+	}
 }
 
-//get "leaderboardArray" from local storage (if it exists) and parse it into a javascript object using JSON.parse
-function getLeaderboard() {
-  var  storedLeaderboard = localStorage.getItem("leaderboardArray");
-  if (storedLeaderboard !== null) {
-    var  leaderboardArray = JSON.parse(storedLeaderboard);
-    return leaderboardArray;
-  } else {
-    leaderboardArray = [];
-  }
-  return leaderboardArray;
+//sort leader's board array from highest to lowest
+function sortLeaderBoard() {
+	var leaderBoardArray = getLeaderBoard();
+	if (!leaderBoardArray) {
+		return;
+	}
+
+	leaderBoardArray.sort(function(a, b) {
+		return b.score - a.score;
+	});
+	return leaderBoardArray;
 }
 
-//display leaderboard on leaderboard card
-function renderLeaderboard() {
-  var  sortedLeaderboardArray = sortLeaderboard();
-  var  highscoreList = document.querySelector("#highscore-list");
-  highscoreList.innerHTML = "";
-  for (var  i = 0; i < sortedLeaderboardArray.length; i++) {
-    var  leaderboardEntry = sortedLeaderboardArray[i];
-    var  newListItem = document.createElement("li");
-    newListItem.textContent =
-      leaderboardEntry.initials + " - " + leaderboardEntry.score;
-    highscoreList.append(newListItem);
-  }
+var clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', clearScores);
+
+//clear local storage and display empty leader's board
+function clearScores() {
+	localStorage.clear();
+	renderLeaderBoard();
 }
 
-//sort leaderboard array from highest to lowest
-function sortLeaderboard() {
-  var  leaderboardArray = getLeaderboard();
-  if (!leaderboardArray) {
-    return;
-  }
+var backButton = document.querySelector('#back-button');
+backButton.addEventListener('click', returnToStart);
 
-  leaderboardArray.sort(function (a, b) {
-    return b.score - a.score;
-  });
-  return leaderboardArray;
-}
-
-var  clearButton = document.querySelector("#clear-button");
-clearButton.addEventListener("click", clearHighscores);
-
-//clear local storage and display empty leaderboard
-function clearHighscores() {
-  localStorage.clear();
-  renderLeaderboard();
-}
-
-var  backButton = document.querySelector("#back-button");
-backButton.addEventListener("click", returnToStart);
-
-//Hide leaderboard card show start card
+//Hide leader's board card show start card
 function returnToStart() {
-  hideCards();
-  startCard.removeAttribute("hidden");
+	hideCards();
+	startCard.removeAttribute('hidden');
 }
 
-//use link to view highscores from any point on the page
-var  leaderboardLink = document.querySelector("#leaderboard-link");
-leaderboardLink.addEventListener("click", showLeaderboard);
+//use link to view scores from any point on the page
+var leaderBoardLink = document.querySelector('#leaderboard-link');
+leaderBoardLink.addEventListener('click', showLeaderBoard);
 
-function showLeaderboard() {
-  hideCards();
-  leaderboardCard.removeAttribute("hidden");
+function showLeaderBoard() {
+	hideCards();
+	leaderBoardCard.removeAttribute('hidden');
 
-  //stop countdown
-  clearInterval(intervalID);
+	//stop countdown
+	clearInterval(intervalID);
 
-  //assign undefined to time and display that, so that time does not appear on page
-  time = undefined;
-  displayTime();
+	//assign undefined to time and display that, so that time does not appear on page
+	time = undefined;
+	displayTime();
 
-  //display leaderboard on leaderboard card
-  renderLeaderboard();
+	//display leader's board on leader's board card
+	renderLeaderBoard();
+}
+
+function renderLeaderBoard() {
+	var sortedLeaderBoardArray = sortLeaderBoard();
+	var scoreList = document.querySelector('#highscore-list');
+	scoreList.innerHTML = '';
+	for (var i = 0; i < sortedLeaderBoardArray.length; i++) {
+		var leaderBoardEntry = sortedLeaderBoardArray[i];
+		var newListItem = document.createElement('li');
+		newListItem.textContent =
+			leaderBoardEntry.initials + ' - ' + leaderBoardEntry.score;
+		scoreList.append(newListItem);
+	}
+}
+
+//sort leader's board array from highest to lowest
+function sortLeaderBoard() {
+	var leaderBoardArray = getLeaderBoard();
+	if (!leaderBoardArray) {
+		return;
+	}
+
+	leaderBoardArray.sort(function(a, b) {
+		return b.score - a.score;
+	});
+	return leaderBoardArray;
+}
+var clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', clearScores);
+
+//clear local storage and display empty leader's board
+function clearScores() {
+	localStorage.clear();
+	renderLeaderBoard();
+}
+var backButton = document.querySelector('#back-button');
+backButton.addEventListener('click', returnToStart);
+
+//hide leader's board card show start card
+function returnToStart() {
+	hideCards();
+	startCard.removeAttribute('hidden');
+}
+//use link to view scores from any point on the page
+var leaderBoardLink = document.querySelector('#leaderboard-link');
+leaderBoardLink.addEventListener('click', showLeaderBoard);
+
+function showLeaderBoard() {
+	hideCards();
+	leaderBoardCard.removeAttribute('hidden');
+
+	//stop countdown
+	clearInterval(intervalID);
+
+	//assign undefined to time and display that, so that time does not appear on page
+	time = undefined;
+	displayTime();
+
+	//display leader's board on leader's board card
+	renderLeaderBoard();
 }
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,20 +1,18 @@
-
 //display scorecard and hide other divs
-var  score = document.querySelector("#score");
-
+var  score = document.querySelector('#score');
 //at end of quiz, clear the timer, hide any visible cards and display the scorecard and display the score as the remaining time
 function endQuiz() {
-  clearInterval(intervalID);
+    clearInterval(intervalID);
   hideCards();
-  scoreCard.removeAttribute("hidden");
+  scoreCard.removeAttribute('hidden');
   score.textContent = time;
 }
 
-var  submitButton = document.querySelector("#submit-button");
-var  inputElement = document.querySelector("#initials");
+var  submitButton = document.querySelector('#submit-button');
+var  inputElement = document.querySelector('#initials');
 
 //store user initials and score when submit button is clicked
-submitButton.addEventListener("click", storeScore);
+submitButton.addEventListener('click', storeScore);
 
 function storeScore(event) {
   //prevent default behaviour of form submission
@@ -22,97 +20,209 @@ function storeScore(event) {
 
   //check for input
   if (!inputElement.value) {
-    alert("Please enter your initials before pressing submit!");
+    alert('Please enter your initials before pressing submit!');
     return;
-  }
-
-  //store score and initials in an object
-  var  leaderboardItem = {
+  };
+   //store score and initials in an object
+   var  leaderBoardItem = {
     initials: inputElement.value,
     score: time,
   };
 
-  updateStoredLeaderboard(leaderboardItem);
+  updateStoredLeaderBoard(leaderBoardItem);
 
-  //hide the question card, display the leaderboardcard
+  //hide the question card, display the leader board card
   hideCards();
-  leaderboardCard.removeAttribute("hidden");
+  leaderBoardCard.removeAttribute('hidden');
 
-  renderLeaderboard();
+  renderLeaderBoard();
+
+//updates the leader's board stored in local storage
+function updateStoredLeaderBoard(leaderBoardItem) {
+  var  leaderBoardArray = getLeaderBoard();
+  //append new leader's board item to leader's board array
+  leaderBoardArray.push(leaderBoardItem);
+  localStorage.setItem('leaderBoardArray', JSON.stringify(leaderBoardArray));
 }
 
-//updates the leaderboard stored in local storage
-function updateStoredLeaderboard(leaderboardItem) {
-  var  leaderboardArray = getLeaderboard();
-  //append new leaderboard item to leaderboard array
-  leaderboardArray.push(leaderboardItem);
-  localStorage.setItem("leaderboardArray", JSON.stringify(leaderboardArray));
-}
-
-//get "leaderboardArray" from local storage (if it exists) and parse it into a javascript object using JSON.parse
-function getLeaderboard() {
-  var  storedLeaderboard = localStorage.getItem("leaderboardArray");
-  if (storedLeaderboard !== null) {
-    var  leaderboardArray = JSON.parse(storedLeaderboard);
-    return leaderboardArray;
+//get ' leaderBoardArray' from local storage (if it exists) and parse it into a javascript object using JSON.parse
+function getLeaderBoard() {
+  var  storedLeaderBoard = localStorage.getItem('leaderBoardArray');
+  if (storedLeaderBoard !== null) {
+    var   leaderBoardArray= JSON.parse(storedLeaderBoard);
+    return  leaderBoardArray;
   } else {
-    leaderboardArray = [];
+    leaderBoardArray = [];
   }
-  return leaderboardArray;
+  return  leaderBoardArray;
+}}
+
+var  submitButton = document.querySelector('#submit-button');
+var  inputElement = document.querySelector('#initials');
+
+//store user initials and score when submit button is clicked
+submitButton.addEventListener('click', storeScore);
+
+function storeScore(event) {
+  //prevent default behaviour of form submission
+  event.preventDefault();
+
+  //check for input
+  if (!inputElement.value) {
+    alert('Please enter your initials before pressing submit!');
+    return;
+  }
+ 
+ //store score and initials in an object
+ var  leaderBoardItem = {
+  initials: inputElement.value,
+  score: time,
+};
+
+updateStoredLeaderBoard(leaderBoardItem);
+
+//hide the question card, display the leader's board card
+hideCards();
+leaderBoardCard.removeAttribute('hidden');
+
+renderLeaderBoard();
 }
 
-//display leaderboard on leaderboard card
-function renderLeaderboard() {
-  var  sortedLeaderboardArray = sortLeaderboard();
-  var  highscoreList = document.querySelector("#highscore-list");
-  highscoreList.innerHTML = "";
-  for (var  i = 0; i < sortedLeaderboardArray.length; i++) {
-    var  leaderboardEntry = sortedLeaderboardArray[i];
-    var  newListItem = document.createElement("li");
+//updates the leader's board stored in local storage
+function updateStoredLeaderBoard(leaderBoardItem) {
+var  leaderBoardArray = getLeaderBoard();
+//append new leader's board item to leader's board array
+leaderBoardArray.push(leaderBoardItem);
+localStorage.setItem('leaderBoardArray', JSON.stringify(leaderBoardArray));
+}
+
+//get 'leaderBoardArray' from local storage (if it exists) and parse it into a javascript object using JSON.parse
+function getLeaderBoard() {
+var  storedLeaderBoard = localStorage.getItem('leaderBoardArray');
+if (storedLeaderBoard !== null) {
+  var  leaderBoardArray = JSON.parse(storedLeaderBoard);
+  return leaderBoardArray;
+} else {
+  leaderBoardArray = [];
+}
+return leaderBoardArray;
+}
+
+//display leader's board on leader's board card
+function renderLeaderBoard() {
+var  sortedLeaderBoardArray = sortLeaderBoard();
+var  highScoreList = document.querySelector('.highscore-list');
+highScoreList.innerHTML = '';
+for (var  i = 0; i < sortedLeaderBoardArray.length; i++) {
+  var  leaderBoardEntry = sortedLeaderBoardArray[i];
+  var  newListItem = document.createElement('li');
+  newListItem.textContent =
+    leaderBoardEntry.initials + ' - ' + leaderBoardEntry.score;
+  highScoreList.append(newListItem);
+}
+}
+
+//sort leader's board array from highest to lowest
+function sortLeaderBoard() {
+var  leaderBoardArray = getLeaderBoard();
+if (!leaderBoardArray) {
+  return;
+}
+
+leaderBoardArray.sort(function (a, b) {
+  return b.score - a.score;
+});
+return leaderBoardArray;
+}
+
+var  clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', clearScores);
+
+//clear local storage and display empty leader's board
+function clearScores() {
+localStorage.clear();
+renderLeaderBoard();
+}
+
+var  backButton = document.querySelector('#back-button');
+backButton.addEventListener('click', returnToStart);
+
+//Hide leader's board card show start card
+function returnToStart() {
+hideCards();
+startCard.removeAttribute('hidden');
+}
+
+//use link to view scores from any point on the page
+var  leaderBoardLink = document.querySelector('.leaderboard-link');
+leaderBoardLink.addEventListener('click', showLeaderboard);
+
+function showLeaderBoard() {
+hideCards();
+leaderBoardCard.removeAttribute('hidden');
+
+//stop countdown
+clearInterval(intervalID);
+
+//assign undefined to time and display that, so that time does not appear on page
+time = undefined;
+displayTime();
+
+//display leader's board on leader's board card
+renderLeaderBoard();
+}
+//display leader's board on leader's board card
+function renderLeaderBoard() {
+  var  sortedLeaderBoardArray = sortLeaderBoard();
+  var  scoreList = document.querySelector('#highscore-list');
+  scoreList.innerHTML = '';
+  for (var  i = 0; i < sortedLeaderBoardArray.length; i++) {
+    var  leaderBoardEntry = sortedLeaderBoardArray[i];
+    var  newListItem = document.createElement('li');
     newListItem.textContent =
-      leaderboardEntry.initials + " - " + leaderboardEntry.score;
-    highscoreList.append(newListItem);
+      leaderBoardEntry.initials + ' - ' + leaderBoardEntry.score;
+    scoreList.append(newListItem);
   }
 }
 
-//sort leaderboard array from highest to lowest
-function sortLeaderboard() {
-  var  leaderboardArray = getLeaderboard();
-  if (!leaderboardArray) {
+//sort leader's board array from highest to lowest
+function sortLeaderBoard() {
+  var  leaderBoardArray = getLeaderBoard();
+  if (!leaderBoardArray) {
     return;
   }
 
-  leaderboardArray.sort(function (a, b) {
+  leaderBoardArray.sort(function (a, b) {
     return b.score - a.score;
   });
-  return leaderboardArray;
+  return leaderBoardArray;
 }
 
-var  clearButton = document.querySelector("#clear-button");
-clearButton.addEventListener("click", clearHighscores);
+var  clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', clearScores);
 
-//clear local storage and display empty leaderboard
-function clearHighscores() {
+//clear local storage and display empty leader's board
+function clearScores() {
   localStorage.clear();
-  renderLeaderboard();
+  renderLeaderBoard();
 }
 
-var  backButton = document.querySelector("#back-button");
-backButton.addEventListener("click", returnToStart);
+var  backButton = document.querySelector('#back-button');
+backButton.addEventListener('click', returnToStart);
 
-//Hide leaderboard card show start card
+//Hide leader's board card show start card
 function returnToStart() {
   hideCards();
-  startCard.removeAttribute("hidden");
+  startCard.removeAttribute('hidden');
 }
 
-//use link to view highscores from any point on the page
-var  leaderboardLink = document.querySelector("#leaderboard-link");
-leaderboardLink.addEventListener("click", showLeaderboard);
+//use link to view scores from any point on the page
+var  leaderBoardLink = document.querySelector('.leaderboard-link');
+leaderBoardLink.addEventListener('click', showLeaderBoard);
 
-function showLeaderboard() {
+function showLeaderBoard() {
   hideCards();
-  leaderboardCard.removeAttribute("hidden");
+  leaderBoardCard.removeAttribute('hidden');
 
   //stop countdown
   clearInterval(intervalID);
@@ -121,6 +231,6 @@ function showLeaderboard() {
   time = undefined;
   displayTime();
 
-  //display leaderboard on leaderboard card
-  renderLeaderboard();
+  //display leader's board on leader's board card
+  renderLeaderBoard();
 }
