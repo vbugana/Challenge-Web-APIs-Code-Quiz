627c6dc35e9d04531e579cf265fe523dd78a1627
vbugana <61995940+vbugana@users.noreply.github.com>
2022-12-06 22:18:33 +0000

    Adding the correct files HTML CSS JAVASCRIPT

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,100 +1,41 @@
-//Var with array and object for questions, store question text, options and answers in an array
+//store question text, options and answers in an array
 var questions = [
   {
-    questionText: 'Commonly used data types DO NOT include:',
-    options: ['1. strings', '2. booleans', '3. alerts', '4. numbers'],
-    answer: '3. alerts',
+    questionText: "Commonly used data types DO NOT include:",
+    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alerts",
   },
   {
-    questionText: 'Arrays in JavaScript can be used to store ______.',
+    questionText: "Arrays in JavaScript can be used to store ______.",
     options: [
-      '1. numbers and strings',
-      '2. other arrays',
-      '3. booleans',
-      '4. all of the above',
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above",
     ],
-    answer: '4. all of the above',
+    answer: "4. all of the above",
   },
   {
     questionText:
-      'String values must be enclosed within _____ when being assigned to variables.',
-    options: ['1. commas', '2. curly brackets', '3. quotes', '4. parentheses'],
-    answer: '3. quotes',
+      "String values must be enclosed within _____ when being assigned to variables.",
+    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+    answer: "3. quotes",
   },
   {
     questionText:
-      'A very useful tool used during development and debugging for printing content to the debugger is:',
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
     options: [
-      '1. JavaScript',
-      '2. terminal/bash',
-      '3. for loops',
-      '4. console.log',
+      "1. JavaScript",
+      "2. terminal/bash",
+      "3. for loops",
+      "4. console.log",
     ],
-    answer: '4. console.log',
+    answer: "4. console.log",
   },
   {
     questionText:
-      'Which of the following is a statement that can be used to terminate a loop, switch or label statement?',
-    options: ['1. break', '2. stop', '3. halt', '4. exit'],
-    answer: '1. break',
+      "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
+    options: ["1. break", "2. stop", "3. halt", "4. exit"],
+    answer: "1. break",
   },
 ];
-
-//display scorecard and hide other divs
-var  score = document.querySelector('#score');
-//at end of quiz, clear the timer, hide any visible cards and display the scorecard and display the score as the remaining time
-function endQuiz() {
-  clearInterval(intervalID);
-  hideCards();
-  scoreCard.removeAttribute('hidden');
-  score.textContent = time;
-}
-
-var  submitButton = document.querySelector('#submit-button');
-var  inputElement = document.querySelector('#initials');
-
-//store user initials and score when submit button is clicked
-submitButton.addEventListener('click', storeScore);
-
-function storeScore(event) {
-  //prevent default behaviour of form submission
-  event.preventDefault();
-
-  //check for input
-  if (!inputElement.value) {
-    alert('Please enter your initials before pressing submit!');
-    return;
-  };
-   //store score and initials in an object
-   var  leaderBoardItem = {
-    initials: inputElement.value,
-    score: time,
-  };
-
-  updateStoredLeaderBoard(leaderBoardItem);
-
-  //hide the question card, display the leader's board card
-  hideCards();
-  leaderBoardCard.removeAttribute('hidden');
-
-  renderLeaderBoard();
-
-//updates the leader's board stored in local storage
-function updateStoredLeaderBoard(leaderBoardItem) {
-  var  leaderBoardArray = getLeaderBoard();
-  //append new leader's board item to leader's board array
-  leaderBoardArray.push(leaderBoardItem);
-  localStorage.setItem('leaderBoardArray', JSON.stringify(leaderBoardArray));
-}
-
-//get 'leaderBoardArray' from local storage (if it exists) and parse it into a javascript object using JSON.parse
-function getLeaderBoard() {
-  var  storedLeaderBoard = localStorage.getItem('leaderBoardArray');
-  if (storedLeaderBoard !== null) {
-    var  leaderBoardArray = JSON.parse(storedLeaderBoard);
-    return leaderBoardArray;
-  } else {
-    leaderBoardArray = [];
-  }
-  return leaderBoardArray;
-}}
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,18 +1,20 @@
+
 //display scorecard and hide other divs
-var  score = document.querySelector('#score');
+var  score = document.querySelector("#score");
+
 //at end of quiz, clear the timer, hide any visible cards and display the scorecard and display the score as the remaining time
 function endQuiz() {
-    clearInterval(intervalID);
+  clearInterval(intervalID);
   hideCards();
-  scoreCard.removeAttribute('hidden');
+  scoreCard.removeAttribute("hidden");
   score.textContent = time;
 }
 
-var  submitButton = document.querySelector('#submit-button');
-var  inputElement = document.querySelector('#initials');
+var  submitButton = document.querySelector("#submit-button");
+var  inputElement = document.querySelector("#initials");
 
 //store user initials and score when submit button is clicked
-submitButton.addEventListener('click', storeScore);
+submitButton.addEventListener("click", storeScore);
 
 function storeScore(event) {
   //prevent default behaviour of form submission
@@ -20,209 +22,97 @@ function storeScore(event) {
 
   //check for input
   if (!inputElement.value) {
-    alert('Please enter your initials before pressing submit!');
+    alert("Please enter your initials before pressing submit!");
     return;
-  };
-   //store score and initials in an object
-   var  leaderBoardItem = {
+  }
+
+  //store score and initials in an object
+  var  leaderboardItem = {
     initials: inputElement.value,
     score: time,
   };
 
-  updateStoredLeaderBoard(leaderBoardItem);
+  updateStoredLeaderboard(leaderboardItem);
 
-  //hide the question card, display the leader board card
+  //hide the question card, display the leaderboardcard
   hideCards();
-  leaderBoardCard.removeAttribute('hidden');
+  leaderboardCard.removeAttribute("hidden");
 
-  renderLeaderBoard();
-
-//updates the leader's board stored in local storage
-function updateStoredLeaderBoard(leaderBoardItem) {
-  var  leaderBoardArray = getLeaderBoard();
-  //append new leader's board item to leader's board array
-  leaderBoardArray.push(leaderBoardItem);
-  localStorage.setItem('leaderBoardArray', JSON.stringify(leaderBoardArray));
+  renderLeaderboard();
 }
 
-//get ' leaderBoardArray' from local storage (if it exists) and parse it into a javascript object using JSON.parse
-function getLeaderBoard() {
-  var  storedLeaderBoard = localStorage.getItem('leaderBoardArray');
-  if (storedLeaderBoard !== null) {
-    var   leaderBoardArray= JSON.parse(storedLeaderBoard);
-    return  leaderBoardArray;
+//updates the leaderboard stored in local storage
+function updateStoredLeaderboard(leaderboardItem) {
+  var  leaderboardArray = getLeaderboard();
+  //append new leaderboard item to leaderboard array
+  leaderboardArray.push(leaderboardItem);
+  localStorage.setItem("leaderboardArray", JSON.stringify(leaderboardArray));
+}
+
+//get "leaderboardArray" from local storage (if it exists) and parse it into a javascript object using JSON.parse
+function getLeaderboard() {
+  var  storedLeaderboard = localStorage.getItem("leaderboardArray");
+  if (storedLeaderboard !== null) {
+    var  leaderboardArray = JSON.parse(storedLeaderboard);
+    return leaderboardArray;
   } else {
-    leaderBoardArray = [];
+    leaderboardArray = [];
   }
-  return  leaderBoardArray;
-}}
-
-var  submitButton = document.querySelector('#submit-button');
-var  inputElement = document.querySelector('#initials');
-
-//store user initials and score when submit button is clicked
-submitButton.addEventListener('click', storeScore);
-
-function storeScore(event) {
-  //prevent default behaviour of form submission
-  event.preventDefault();
-
-  //check for input
-  if (!inputElement.value) {
-    alert('Please enter your initials before pressing submit!');
-    return;
-  }
- 
- //store score and initials in an object
- var  leaderBoardItem = {
-  initials: inputElement.value,
-  score: time,
-};
-
-updateStoredLeaderBoard(leaderBoardItem);
-
-//hide the question card, display the leader's board card
-hideCards();
-leaderBoardCard.removeAttribute('hidden');
-
-renderLeaderBoard();
+  return leaderboardArray;
 }
 
-//updates the leader's board stored in local storage
-function updateStoredLeaderBoard(leaderBoardItem) {
-var  leaderBoardArray = getLeaderBoard();
-//append new leader's board item to leader's board array
-leaderBoardArray.push(leaderBoardItem);
-localStorage.setItem('leaderBoardArray', JSON.stringify(leaderBoardArray));
-}
-
-//get 'leaderBoardArray' from local storage (if it exists) and parse it into a javascript object using JSON.parse
-function getLeaderBoard() {
-var  storedLeaderBoard = localStorage.getItem('leaderBoardArray');
-if (storedLeaderBoard !== null) {
-  var  leaderBoardArray = JSON.parse(storedLeaderBoard);
-  return leaderBoardArray;
-} else {
-  leaderBoardArray = [];
-}
-return leaderBoardArray;
-}
-
-//display leader's board on leader's board card
-function renderLeaderBoard() {
-var  sortedLeaderBoardArray = sortLeaderBoard();
-var  highScoreList = document.querySelector('.highscore-list');
-highScoreList.innerHTML = '';
-for (var  i = 0; i < sortedLeaderBoardArray.length; i++) {
-  var  leaderBoardEntry = sortedLeaderBoardArray[i];
-  var  newListItem = document.createElement('li');
-  newListItem.textContent =
-    leaderBoardEntry.initials + ' - ' + leaderBoardEntry.score;
-  highScoreList.append(newListItem);
-}
-}
-
-//sort leader's board array from highest to lowest
-function sortLeaderBoard() {
-var  leaderBoardArray = getLeaderBoard();
-if (!leaderBoardArray) {
-  return;
-}
-
-leaderBoardArray.sort(function (a, b) {
-  return b.score - a.score;
-});
-return leaderBoardArray;
-}
-
-var  clearButton = document.querySelector('#clear-button');
-clearButton.addEventListener('click', clearScores);
-
-//clear local storage and display empty leader's board
-function clearScores() {
-localStorage.clear();
-renderLeaderBoard();
-}
-
-var  backButton = document.querySelector('#back-button');
-backButton.addEventListener('click', returnToStart);
-
-//Hide leader's board card show start card
-function returnToStart() {
-hideCards();
-startCard.removeAttribute('hidden');
-}
-
-//use link to view scores from any point on the page
-var  leaderBoardLink = document.querySelector('.leaderboard-link');
-leaderBoardLink.addEventListener('click', showLeaderboard);
-
-function showLeaderBoard() {
-hideCards();
-leaderBoardCard.removeAttribute('hidden');
-
-//stop countdown
-clearInterval(intervalID);
-
-//assign undefined to time and display that, so that time does not appear on page
-time = undefined;
-displayTime();
-
-//display leader's board on leader's board card
-renderLeaderBoard();
-}
-//display leader's board on leader's board card
-function renderLeaderBoard() {
-  var  sortedLeaderBoardArray = sortLeaderBoard();
-  var  scoreList = document.querySelector('#highscore-list');
-  scoreList.innerHTML = '';
-  for (var  i = 0; i < sortedLeaderBoardArray.length; i++) {
-    var  leaderBoardEntry = sortedLeaderBoardArray[i];
-    var  newListItem = document.createElement('li');
+//display leaderboard on leaderboard card
+function renderLeaderboard() {
+  var  sortedLeaderboardArray = sortLeaderboard();
+  var  highscoreList = document.querySelector("#highscore-list");
+  highscoreList.innerHTML = "";
+  for (var  i = 0; i < sortedLeaderboardArray.length; i++) {
+    var  leaderboardEntry = sortedLeaderboardArray[i];
+    var  newListItem = document.createElement("li");
     newListItem.textContent =
-      leaderBoardEntry.initials + ' - ' + leaderBoardEntry.score;
-    scoreList.append(newListItem);
+      leaderboardEntry.initials + " - " + leaderboardEntry.score;
+    highscoreList.append(newListItem);
   }
 }
 
-//sort leader's board array from highest to lowest
-function sortLeaderBoard() {
-  var  leaderBoardArray = getLeaderBoard();
-  if (!leaderBoardArray) {
+//sort leaderboard array from highest to lowest
+function sortLeaderboard() {
+  var  leaderboardArray = getLeaderboard();
+  if (!leaderboardArray) {
     return;
   }
 
-  leaderBoardArray.sort(function (a, b) {
+  leaderboardArray.sort(function (a, b) {
     return b.score - a.score;
   });
-  return leaderBoardArray;
+  return leaderboardArray;
 }
 
-var  clearButton = document.querySelector('#clear-button');
-clearButton.addEventListener('click', clearScores);
+var  clearButton = document.querySelector("#clear-button");
+clearButton.addEventListener("click", clearHighscores);
 
-//clear local storage and display empty leader's board
-function clearScores() {
+//clear local storage and display empty leaderboard
+function clearHighscores() {
   localStorage.clear();
-  renderLeaderBoard();
+  renderLeaderboard();
 }
 
-var  backButton = document.querySelector('#back-button');
-backButton.addEventListener('click', returnToStart);
+var  backButton = document.querySelector("#back-button");
+backButton.addEventListener("click", returnToStart);
 
-//Hide leader's board card show start card
+//Hide leaderboard card show start card
 function returnToStart() {
   hideCards();
-  startCard.removeAttribute('hidden');
+  startCard.removeAttribute("hidden");
 }
 
-//use link to view scores from any point on the page
-var  leaderBoardLink = document.querySelector('.leaderboard-link');
-leaderBoardLink.addEventListener('click', showLeaderBoard);
+//use link to view highscores from any point on the page
+var  leaderboardLink = document.querySelector("#leaderboard-link");
+leaderboardLink.addEventListener("click", showLeaderboard);
 
-function showLeaderBoard() {
+function showLeaderboard() {
   hideCards();
-  leaderBoardCard.removeAttribute('hidden');
+  leaderboardCard.removeAttribute("hidden");
 
   //stop countdown
   clearInterval(intervalID);
@@ -231,6 +121,6 @@ function showLeaderBoard() {
   time = undefined;
   displayTime();
 
-  //display leader's board on leader's board card
-  renderLeaderBoard();
+  //display leaderboard on leaderboard card
+  renderLeaderboard();
 }
